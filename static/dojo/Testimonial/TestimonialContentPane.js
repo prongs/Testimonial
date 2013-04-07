fc93ccae0a43f5e40e2e9a912625d2d3852d5149
Rajat Khandelwal <rajatgupta59@gmail.com>
2013-04-07 21:09:52 +0530

    save feedback to the user, close confirmation

--- static/dojo/Testimonial/TestimonialContentPane.js
+++ static/dojo/Testimonial/TestimonialContentPane.js
@@ -11,6 +11,9 @@ define([
                 return declare('testimonial.TestimonialContentPane', [ ContentPane, _TemplatedMixin, _WidgetsInTemplateMixin ], {
                         templateString: template,
                         constructor: function(args){
+                            var self = this;
+                            self.friend_name = args.title;
+                            self.inherited(arguments);
                         },
                         postCreate: function(){
                             var self = this;
@@ -30,8 +33,11 @@ define([
                         },
                         load_write_testimonial: function(response, ioArgs){
                             var self = this;
-                            self.editor.set('value', response.content);
+                            self.savedValue = response.content;
+                            self.saved = true;
+                            self.editor.set('value', self.savedValue);
                             self.editor.setDisabled(false);
+                            setTimeout(function(){self.checkSavedStatus();}, 1);
                         },
                         error_load_write_testimonial: function(error, ioArgs){
                             var self = this;
@@ -45,15 +51,16 @@ define([
                         },
                         error_load_read_testimonial: function(error, ioArgs){
                             var self = this;
-                          self.read_pane.containerNode.innerHTML = error;
+                            self.read_pane.containerNode.innerHTML = error;
                         },
                         getCookie: function(name) {
                             var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
                             return r ? r[1] : undefined;
                         },
                         save: function(args){
-                            var self =this;
-                            content = {content: self.editor.getValue(), _xsrf: self.getCookie('_xsrf')};
+                            var self = this;
+                            self.savedValue = self.editor.get('value');
+                            content = {content: self.savedValue, _xsrf: self.getCookie('_xsrf')};
                             xhr.post({
                                 url: "/write/" + self.friend_id,
                                 content:content,
@@ -63,13 +70,24 @@ define([
                         },
                         save_xhr_success: function(data, ioArgs){
                             var self = this;
-                            console.log("saved!");
-                            console.log(data);
+                            self.saved = true;
+                            self.set('title', self.friend_name);
+                            setTimeout(function(){self.checkSavedStatus();}, 1);
                         },
                         save_xhr_error: function(err, ioArgs){
                             var self = this;
                             console.log("Error!");
                             console.log(err);
+                        },
+                        checkSavedStatus: function(){
+                            var self = this;
+                            if(self.editor.get('value') == self.savedValue)
+                            {
+                                setTimeout(function(){self.checkSavedStatus();}, 1);
+                                return;
+                            }
+                            self.set('title', self.friend_name + "&nbsp;&nbsp;<sup>*</sup>");
+                            self.saved = false;
                         }
                 });
         }
